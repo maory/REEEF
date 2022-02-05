afdd897ef854e97f15a4b6f408b190f0fb265c2b
Maor Yaffe <maoryf1@gmail.com>
2022-02-05 21:44:18 +0200

    Add mock data and update the schema

--- reef/src/service/storefront/storefront.service.ts
+++ reef/src/service/storefront/storefront.service.ts
@@ -7,4 +7,8 @@ export class StorefrontService {
     getAllStoreFronts() : Storefront[] {
         return [{ id: '1', name : "storefront1", zipCode: 1122,address : "storefront1 address", imageUrl: "imageUrl" }];
     }
+
+    getAllStoreFrontsInMyArea(zipCode: number) {
+        return [{ id: '1', name : "storefront1", zipCode: 1122,address : "storefront1 address", imageUrl: "imageUrl" }];
+    }
 }
